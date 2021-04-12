cfb080defff292336b0da6e0509f57c08b84f708
Yara Polana <yara.polana@gmail.com>
2021-04-12 20:03:10 +0000

    refactor App

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,32 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from './Button'
+
+interface SidebarProps {
+  genres: {
+    id: number
+    name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family'
+    title: string
+  }[]
+  handleClickButton(genreId: number): void
+  selectedGenreId: number
+}
+
+export function Sidebar({ genres, handleClickButton, selectedGenreId }: SidebarProps) {
+  return(
+    <nav className="sidebar">
+        <span>Watch<p>Me</p></span>
+
+        <div className="buttons-container">
+          {genres.map(genre => (
+            <Button
+              key={String(genre.id)}
+              title={genre.title}
+              iconName={genre.name}
+              onClick={() => handleClickButton(genre.id)}
+              selected={selectedGenreId === genre.id}
+            />
+          ))}
+        </div>
+
+      </nav>
+  )
 }
