834e0776f23ca1c1690a6cabf0591832f0b51900
Yara Polana <yara.polana@gmail.com>
2021-04-15 15:19:15 +0000

    cleanup

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -13,20 +13,19 @@ interface SidebarProps {
 export function Sidebar({ genres, handleClickButton, selectedGenreId }: SidebarProps) {
   return(
     <nav className="sidebar">
-        <span>Watch<p>Me</p></span>
+      <span>Watch<p>Me</p></span>
 
-        <div className="buttons-container">
-          {genres.map(genre => (
-            <Button
-              key={String(genre.id)}
-              title={genre.title}
-              iconName={genre.name}
-              onClick={() => handleClickButton(genre.id)}
-              selected={selectedGenreId === genre.id}
-            />
-          ))}
-        </div>
-
-      </nav>
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
   )
 }
